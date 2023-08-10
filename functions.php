49d02057b9e39d0912b7d2b952e037c0f37d7350
张洪Heo <qewan@foxmail.com>
2023-08-10 14:06:17 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,24 +1,5 @@
 <?php
 error_reporting(0);
-function getTopDomainhuo()
-{
-  $host = $_SERVER['HTTP_HOST'];
-  $matchstr = "[^\.]+\.(?:(" . $str . ")|\w{2}|((" . $str . ")\.\w{2}))$";
-  if (preg_match("/" . $matchstr . "/ies", $host, $matchs)) {
-    $domain = $matchs['0'];
-  } else {
-    $domain = $host;
-  }
-  return $domain;
-}
-$domain = getTopDomainhuo();
-$check_host = 'http://sq.zmki.cn/update.php';
-$client_check = $check_host . '?a=client_check&u=' . $_SERVER['HTTP_HOST'];
-$check_message = $check_host . '?a=check_message&u=' . $_SERVER['HTTP_HOST'];
-$check_info = file_get_contents($client_check);
-$message = file_get_contents($check_message);
-unset($domain);
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 function themeConfig($form)
 {
   if ($check_info == '1') {
